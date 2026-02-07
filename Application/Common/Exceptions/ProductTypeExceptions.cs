2eb1bbc214de46d55af8dc01eb08ff469864a8b6
NRM <roman.nikitchuk123@gmail.com>
2026-02-07 20:06:49 +0100

    feat: add update and delete commads additional exceptions

--- Application/Common/Exceptions/ProductTypeExceptions.cs
+++ Application/Common/Exceptions/ProductTypeExceptions.cs
@@ -9,3 +9,6 @@ public class ProductTypeNotFoundException(int productTypeId)
 
 public class UnhandledProductTypeException(int productTypeId, Exception? innerException = null)
     : BaseException(productTypeId, "Unhandled product type exception", innerException);
+
+public class ProductTypeHasProductsException(int productTypeId)
+    : BaseException(productTypeId, $"ProductType with id {productTypeId} has related products and cannot be deleted");
---
+++ Application/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Queries;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+using LanguageExt;
+using MediatR;
+
+namespace Application.Products.Commands;
+
+public class DeleteProductCommand : IRequest<Either<BaseException, Product>>
+{
+    public int Id { get; init; }
+}
+
+public class DeleteProductCommandHandler(
+    IRepository<Product> productRepository,
+    IProductQueries productQueries)
+    : IRequestHandler<DeleteProductCommand, Either<BaseException, Product>>
+{
+    public async Task<Either<BaseException, Product>> Handle(
+        DeleteProductCommand request,
+        CancellationToken cancellationToken)
+    {
+        var product = await productQueries.GetByIdAsync(request.Id, cancellationToken);
+
+        return await product.MatchAsync(
+            p => DeleteEntity(p, cancellationToken),
+            () => new ProductNotFoundException(request.Id));
+    }
+
+    private async Task<Either<BaseException, Product>> DeleteEntity(
+        Product product,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            await productRepository.DeleteAsync(product, cancellationToken);
+            return product;
+        }
+        catch (Exception ex)
+        {
+            return new UnhandledProductException(product.Id, ex);
+        }
+    }
+}
