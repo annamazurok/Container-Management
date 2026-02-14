8cadd512cfb344240ecbbda6ecd4c20f46550579
annamazurok <mazurokanna25@gmail.com>
2026-02-15 01:05:10 +0200

    feat: add migrations

---
+++ Infrastructure/Persistence/MigrationsDirectory/20260213142310_Initial.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.MigrationsDirectory
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "product_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "varchar(255)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_product_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "units",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "varchar(100)", nullable: false),
+                    unit_type = table.Column<string>(type: "varchar(50)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_units", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type_id = table.Column<int>(type: "integer", nullable: false),
+                    produced = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expiration_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    description = table.Column<string>(type: "varchar(1000)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_products", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_products_product_types_type_id",
+                        column: x => x.type_id,
+                        principalTable: "product_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    surname = table.Column<string>(type: "varchar(100)", nullable: false),
+                    fathers_name = table.Column<string>(type: "varchar(100)", nullable: true),
+                    role_id = table.Column<int>(type: "integer", nullable: false),
+                    confirmed = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_users_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "container_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    volume = table.Column<int>(type: "int", nullable: false),
+                    unit_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_container_types", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_container_types_units_unit_id",
+                        column: x => x.unit_id,
+                        principalTable: "units",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "container_type_product_types",
+                columns: table => new
+                {
+                    container_type_id = table.Column<int>(type: "integer", nullable: false),
+                    product_type_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    created_by = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_container_type_product_types", x => new { x.container_type_id, x.product_type_id });
+                    table.ForeignKey(
+                        name: "fk_container_type_product_types_container_type_id",
+                        column: x => x.container_type_id,
+                        principalTable: "container_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_container_type_product_types_product_type_id",
+                        column: x => x.product_type_id,
+                        principalTable: "product_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "containers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    code = table.Column<string>(type: "varchar(100)", nullable: false),
+                    type_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: true),
+                    status = table.Column<string>(type: "varchar(50)", nullable: false),
+                    changing_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: true),
+                    unit_id = table.Column<int>(type: "integer", nullable: true),
+                    notes = table.Column<string>(type: "varchar(500)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_containers", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_containers_container_types_type_id",
+                        column: x => x.type_id,
+                        principalTable: "container_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_containers_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_containers_units_unit_id",
+                        column: x => x.unit_id,
+                        principalTable: "units",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "histories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    container_id = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: true),
+                    unit_id = table.Column<int>(type: "integer", nullable: true),
+                    product_id = table.Column<int>(type: "integer", nullable: true),
+                    action_type = table.Column<string>(type: "varchar(50)", nullable: false),
+                    from_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    notes = table.Column<string>(type: "varchar(500)", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "timezone('utc', now())"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<int>(type: "integer", nullable: false),
+                    changed_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_histories", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_histories_containers_container_id",
+                        column: x => x.container_id,
+                        principalTable: "containers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_histories_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_histories_units_unit_id",
+                        column: x => x.unit_id,
+                        principalTable: "units",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_container_type_product_types_product_type_id",
+                table: "container_type_product_types",
+                column: "product_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_container_types_unit_id",
+                table: "container_types",
+                column: "unit_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_containers_product_id",
+                table: "containers",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_containers_type_id",
+                table: "containers",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_containers_unit_id",
+                table: "containers",
+                column: "unit_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_histories_container_id",
+                table: "histories",
+                column: "container_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_histories_product_id",
+                table: "histories",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_histories_unit_id",
+                table: "histories",
+                column: "unit_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_products_type_id",
+                table: "products",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_role_id",
+                table: "users",
+                column: "role_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "container_type_product_types");
+
+            migrationBuilder.DropTable(
+                name: "histories");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "containers");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "container_types");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "units");
+
+            migrationBuilder.DropTable(
+                name: "product_types");
+        }
+    }
+}
